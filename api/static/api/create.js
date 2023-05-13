9f2c92ce59d5f960dceffb20126e1224a72dbd3e
OsafAliSayed <99737087+OsafAliSayed@users.noreply.github.com>
2023-05-13 09:47:56 +0530

    Adding comments to create.js

--- api/static/api/create.js
+++ api/static/api/create.js
@@ -1,6 +1,9 @@
+//select the form from front end
 var createForm = document.querySelector(".createForm");
 
+// Adding submit event listener
 createForm.addEventListener("submit", (e) => {
+    // Prevent default page refreshing 
     e.preventDefault();
     var book = document.querySelector("#title").value;
     var author = document.querySelector("#author").value;
@@ -12,11 +15,15 @@ createForm.addEventListener("submit", (e) => {
     // PUT request to update the data
     xhr.open('POST', `/books`, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    //creating a parameter i.e simple a string of object required by api to create new object.
     var param = `{
         "name": "${book}",
         "author": "${author}",
         "year": "${year}"
     }`;
+    
+    //if the request is successfull redirect to homepage
     xhr.onload = function() {
         console.log("Request sent");
         if (this.status == 201) {
@@ -24,6 +31,8 @@ createForm.addEventListener("submit", (e) => {
             window.location.href = "/";
         }
     }
+    
+    //send the request with parameter(param)
     xhr.send(param);
 
 
