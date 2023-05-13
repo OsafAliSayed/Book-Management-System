f68fafb872a46886257535aa5d09bee6fbb4ec46
OsafAliSayed <99737087+OsafAliSayed@users.noreply.github.com>
2023-05-13 09:45:00 +0530

    adding comments to edit.js

--- api/static/api/edit.js
+++ api/static/api/edit.js
@@ -1,7 +1,12 @@
+// get the edit form
 var editForm = document.querySelector(".editForm");
 
+// Eventlistener for submit 
 editForm.addEventListener("submit", (e) => {
+    // prevent default refreshing on form submit    
     e.preventDefault();
+    
+    // Getting all the required data
     var id = editForm.id;
     var book = document.querySelector("#title").value;
     var author = document.querySelector("#author").value;
@@ -13,12 +18,15 @@ editForm.addEventListener("submit", (e) => {
     // PUT request to update the data
     xhr.open('PUT', `/book/${id}`, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    // param - string of object that has to be sent to api request.     
     var param = `{
         "id": ${id},
         "name": "${book}",
         "author": "${author}",
         "year": "${year}"
     }`;
+    // Reloading page on successfull submission
     xhr.onload = function() {
         console.log("Request sent");
         if (this.status == 200) {
@@ -26,6 +34,8 @@ editForm.addEventListener("submit", (e) => {
             window.location.href = "/";
         }
     }
+    
+    //sending the request
     xhr.send(param);
 
 
